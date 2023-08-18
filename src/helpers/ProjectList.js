b33053e12e29a8d471f10d063e0a3f54416b9276
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-18 15:50:18 +0200

    UPDATE Projects
    Updated list of projects. Removed one concept project and replaced it with a real project.

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,75 +1,74 @@
-import proj1 from '../Assets/myWorkoutApp.png';
-import proj2 from '../Assets/budgetApp.png';
-import proj3 from '../Assets/padelTennis.jpg';
-import proj4 from '../Assets/SpotiyClone.png';
-import proj5 from '../Assets/bitCoinMining.jpg';
-import proj6 from '../Assets/TicTacToeGame.png';
+import proj1 from "../Assets/myWorkoutApp.png";
+import proj2 from "../Assets/gmn.png"
+import proj3 from "../Assets/padelTennis.jpg";
+import proj4 from "../Assets/budgetApp.png";
+import proj5 from "../Assets/bitCoinMining.jpg";
+import proj6 from "../Assets/TicTacToeGame.png";
 
 export const ProjectList = [
-    {
-        name: "Fitness App",
-        image: proj1,
-        status: "Status: ",
-        status2: "Almost done",
-        title: "Tools",
-        skills: "Swift, Xcode, Figma, Github & App Store Connect",
-        link: "https://github.com/NicolaiJGangdal/FitnessApp",
-        link2: "https://www.apple.com/app-store/"
-    },
+  {
+    name: "Fitness App",
+    image: proj1,
+    status: "Status: ",
+    status2: "Almost done",
+    title: "Tools",
+    skills: "Swift, Xcode, Figma, Github & App Store Connect",
+    link: "https://github.com/NicolaiJGangdal/FitnessApp",
+    link2: "https://www.apple.com/app-store/",
+  },
 
-    {
-        name: "Personal Budgetting app",
-        image: proj2,
-        status: "Status: ",
-        status2: "Planning phase",
-        title: "Tools",
-        skills: "Figma",
-        link: "https://github.com/NicolaiJGangdal?tab=repositories",
-        link2: "https://www.apple.com/app-store/"
-    },
+  {
+    name: "Guess My Number App",
+    image: proj2,
+    status: "Status: ",
+    status2: "Done",
+    title: "Tools",
+    skills: "React Native, Expo, Figma, Github",
+    link: "https://github.com/NicolaiJGangdal/GuessNumberGame",
+    link2: "https://www.apple.com/app-store/",
+  },
 
-    
-    {
-        name: "Padel Americano App",
-        image: proj3,
-        status: "Status: ",
-        status2: "In Planning Stages",
-        title: "Tools",
-        skills: "Figma",
-        link: "https://github.com/NicolaiJGangdal?tab=repositories",
-        link2: "https://www.apple.com/app-store/"
-    },
+  {
+    name: "Padel Americano App",
+    image: proj3,
+    status: "Status: ",
+    status2: "In Planning Stages",
+    title: "Tools",
+    skills: "Figma",
+    link: "https://github.com/NicolaiJGangdal?tab=repositories",
+    link2: "https://www.apple.com/app-store/",
+  },
 
-    {
-        name: "Spotify Clone",
-        image: proj4,
-        status: "Status: ",
-        status2: "Will not be made",
-        title: "Concept app",
-        skills: "Fake project to fill out space",
-        link: "https://github.com/NicolaiJGangdal?tab=repositories",
-        link2: "https://www.apple.com/app-store/"
-    },
+  {
+    name: "Personal Budgetting app",
+    image: proj4,
+    status: "Status: ",
+    status2: "Planning phase",
+    title: "Tools",
+    skills: "Figma",
+    link: "https://github.com/NicolaiJGangdal?tab=repositories",
+    link2: "https://www.apple.com/app-store/",
+  },
 
-    {
-        name: "Bitcoin Mining WebSite",
-        image: proj5,
-        status: "Status: ",
-        status2: "Will not be made",
-        title: "Concept app",
-        skills: "Fake project to fill out space",
-        link: "https://github.com/NicolaiJGangdal?tab=repositories",
-        link2: "https://www.apple.com/app-store/"
-    },
+  {
+    name: "Bitcoin Mining WebSite",
+    image: proj5,
+    status: "Status: ",
+    status2: "Will not be made",
+    title: "Concept app",
+    skills: "Fake project to fill out space",
+    link: "https://github.com/NicolaiJGangdal?tab=repositories",
+    link2: "https://www.apple.com/app-store/",
+  },
 
-    {
-        name: "TicTacToe Game",
-        image: proj6,
-        status: "Status :",
-        status2: "Under developement",
-        title: "Tools",
-        skills: "Xcode & Swift",
-        link: "https://github.com/NicolaiJGangdal?tab=repositories",
-        link2: "https://www.apple.com/app-store/"
-    },
-]
+  {
+    name: "TicTacToe Game",
+    image: proj6,
+    status: "Status :",
+    status2: "Under developement",
+    title: "Tools",
+    skills: "Xcode & Swift",
+    link: "https://github.com/NicolaiJGangdal?tab=repositories",
+    link2: "https://www.apple.com/app-store/",
+  },
+];
