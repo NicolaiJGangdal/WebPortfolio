cd44f8793cdeb2d25710c7ef45471177d98d81f7
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-22 10:19:05 +0200

    UPDATE
    I have updated the list of skills on the Home-page. I have translated my experiences on the experience from norwegian to english

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,45 @@
 import React from "react";
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import EmailIcon from '@material-ui/icons/Email';
-import GithubIcon from '@material-ui/icons/GitHub';
-import "../styles/Home.css"
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="about">
-                <h2>Hi, My Name is Nicolai</h2>
-                <div className="prompt"> 
-                    <p>A software developer with a passion for learning and creating.</p>
-                    <LinkedInIcon />
-                    <EmailIcon />
-                    <GithubIcon />
-                </div>
-            </div>
-            <div className="skills">
-                <h1> Skills</h1>
-                <ol className="list">
-                    <li className="item">
-                        <h2>Frontend</h2>
-                        <span>
-                            React, Angular, Redux, HTML, CSS, <br /> NPM, Yarn, Ionic, Bootstrap, <br/> MaterialUI, TailwindCSS
-                        </span>
-                    </li> 
-                    <li className="item">
-                        <h2>Backend</h2>
-                        <span>
-                            Java, Swift, .NET, GraphQL, C#, F#, <br />SQL, MySQL, Firebase 
-                        </span>
-                    </li> 
-                </ol>
-            </div>
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Nicolai</h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
         </div>
-    );
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Frontend</h2>
+            <span>
+              React, React Native, Swift, <br />
+              Angular, Typescript, Javascript, <br /> 
+              Redux, HTML, CSS, <br /> 
+              NPM, Yarn, Ionic, Bootstrap, <br />
+              MaterialUI, TailwindCSS
+            </span>
+          </li>
+          <li className="item">
+            <h2>Backend</h2>
+            <span>
+              Java, C#, GraphQL, <br />
+              SQL, MySQL
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
