15553f4bc0fed466f0a374dd27b1cf410834ad46
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-08-25 12:21:11 +0200

    ADD Icon and link in footer
    Updated the react-icons package and added the Strava-logo to the footer. The footer is added with a link that opens Strava in a new tab.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,24 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { FaStrava } from "react-icons/fa";
 import "../styles/Footer.css";
 
 function Footer() {
     return (
         <div className="footer">
             <div className="socialMedia">
+                <Link to="https://www.facebook.com/klubelu/?locale=nb_NO" target="_blank">
+                    <FacebookIcon />
+                </Link>
                 <Link to="https://www.instagram.com/nicolai.gangdal/" target="_blank">
                     <InstagramIcon />
                 </Link>
+                <Link to="https://www.strava.com/athletes/67436206" target="_blank">
+                    <FaStrava />
+                </Link>
                 <Link to="https://twitter.com/nicolaijgangdal" target="_blank">
                     <TwitterIcon />
-                </Link>
-                <Link to="https://www.facebook.com/klubelu/?locale=nb_NO" target="_blank">
-                    <FacebookIcon />
                 </Link>
                 <Link to="https://www.linkedin.com/in/nicolai-jensen-gangdal-223651219/?originalSubdomain=no" target="_blank">
                     <LinkedInIcon />
